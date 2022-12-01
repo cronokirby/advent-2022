bd49482ca48e7c69a93393935d9f30eb1524ca19
Lúcás C. Meier <lucas@cronokirby.com>
2022-12-01 18:23:38 +0100

    Answer 001

--- src/solutions/s001.rs
+++ src/solutions/s001.rs
@@ -2,11 +2,44 @@ use std::error::Error;
 
 use crate::advent::Solution;
 
+#[derive(Debug)]
+struct Input {
+    calories: Vec<Vec<u64>>,
+}
+
+impl Input {
+    fn parse(data: &str) -> Self {
+        let mut out = vec![Vec::new()];
+        for line in data.lines() {
+            if let Ok(x) = line.parse::<u64>() {
+                out.last_mut().unwrap().push(x);
+            } else {
+                out.push(Vec::new())
+            }
+        }
+        Input { calories: out }
+    }
+}
+
+fn solve_A(input: &Input) -> Result<u64, Box<dyn Error>> {
+    input.calories.iter().map(|v| v.iter().sum()).max().ok_or_else(|| "empty calories".into())
+}
+
+fn solve_B(input: &Input) -> u64 {
+    let mut sums: Vec<u64> = input.calories.iter().map(|v| v.iter().sum()).collect();
+    sums.sort();
+    sums.reverse();
+    sums[..3].iter().sum()
+}
+
 struct Solution001;
 
 impl Solution for Solution001 {
     fn answer(&self, data: &str) -> Result<(String, String), Box<dyn Error>> {
-        Ok(("A".to_owned(), "B".to_owned()))
+        let input = Input::parse(data);
+        let a = solve_A(&input)?;
+        let b = solve_B(&input);
+        Ok((format!("{}", a), format!("{}", b)))
     }
 }
 
