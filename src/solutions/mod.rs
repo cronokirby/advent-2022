8868b28de94fae092a717a011ef4ceafd5015a74
Lúcás C. Meier <lucas@cronokirby.com>
2022-12-03 19:08:43 +0100

    Day 3 part 1

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -2,7 +2,8 @@ use crate::advent::Solution;
 
 mod s001;
 mod s002;
+mod s003;
 
 pub fn solutions() -> Vec<Box<dyn Solution>> {
-    vec![s001::solution(), s002::solution()]
+    vec![s001::solution(), s002::solution(), s003::solution()]
 }
---
+++ src/solutions/s003.rs
@@ -0,0 +1,87 @@
+use std::{error::Error, collections::HashSet};
+
+use crate::advent::Solution;
+
+type Priority = u8;
+
+fn parse_priority(c: char) -> Result<Priority, Box<dyn Error>> {
+    if c.is_ascii_lowercase() {
+        Ok(c as u8 - b'a' + 1)
+    } else if c.is_ascii_uppercase() {
+        Ok(c as u8 - b'A' + 27)
+    } else {
+        Err(format!("unknown priority {}", c).into())
+    }
+}
+
+#[derive(Debug)]
+struct Arrangement {
+    left: Vec<Priority>,
+    right: Vec<Priority>
+}
+
+impl Arrangement {
+    fn parse(data: &str) -> Result<Self, Box<dyn Error>> {
+        if data.len() % 2 != 0 {
+            return Err("line was not even".into());
+        }
+        let half_len = data.len() / 2;
+        let mut left = Vec::with_capacity(half_len);
+        let mut right = Vec::with_capacity(half_len);
+        for (i, c) in data.chars().enumerate() {
+            let parsed = parse_priority(c)?;
+            if i < half_len {
+                left.push(parsed);
+            } else {
+                right.push(parsed);
+            }
+        }
+        Ok(Arrangement { left, right })
+    }
+}
+
+#[derive(Debug)]
+struct Input(Vec<Arrangement>);
+
+impl Input {
+    fn parse(data: &str) -> Result<Self, Box<dyn Error>> {
+        let mut out = Vec::new();
+        for line in data.lines() {
+            out.push(Arrangement::parse(line)?);
+        }
+        Ok(Input(out))
+    }
+}
+
+fn solve_a(input: &Input) -> u64 {
+    let mut sum = 0;
+    for arr in &input.0 {
+        let left_set: HashSet<u8> = HashSet::from_iter(arr.left.iter().copied());
+        for r in &arr.right {
+            if left_set.contains(r) {
+                sum += u64::from(*r);
+                break;
+            }
+        }
+    }
+    sum
+}
+
+fn solve_b(input: &Input) -> u64 {
+    0
+}
+
+struct Solution003;
+
+impl Solution for Solution003 {
+    fn answer(&self, data: &str) -> Result<(String, String), Box<dyn Error>> {
+        let input = Input::parse(data)?;
+        let a = solve_a(&input);
+        let b = solve_b(&input);
+        Ok((format!("{}", a), format!("{}", b)))
+    }
+}
+
+pub fn solution() -> Box<dyn Solution> {
+    Box::new(Solution003)
+}
